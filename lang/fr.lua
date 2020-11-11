188f9abc7a7e07fda2897c71bd881d305937ca2a
Tom Cumbow <tcumbow@icloud.com>
2020-11-11 16:41:38 -0500

    Initial commit

---
+++ lang/fr.lua
@@ -0,0 +1,9 @@
+﻿-- by @Frackou
+
+ZO_CreateStringId("SI_VOTANS_FILET_FISH_ALL", "Ecailler")
+ZO_CreateStringId("SI_KEYBINDINGS_CATEGORY_FILET_FISH", "Fish Fillet")
+ZO_CreateStringId("SI_BINDING_NAME_VOTANS_FISH_FILLET_ALL_STACKS", "Ecailler")
+ZO_CreateStringId("SI_VOTANS_FILET_OPT_ALLSTACKS", "Ecailler une seule pile de poissons")
+ZO_CreateStringId("SI_VOTANS_FILET_OPT_ALLSTACKS_TOOLTIP", "Si activé, Votan's Fish Fillet écaillera automatiquement la pile de poissons sélectionnée.")
+ZO_CreateStringId("SI_VOTANS_FILET_OPT_ALLSTACKS_ALWAYS", "Ecailler toutes les piles de poissons")
+ZO_CreateStringId("SI_VOTANS_FILET_OPT_ALLSTACKS_ALWAYS_TOOLTIP", "Si activé, Votan's Fish Fillet écaillera automatiquement toutes vos piles de poissons les unes après les autres.")
---
+++ lang/ru.lua
@@ -0,0 +1,7 @@
+SafeAddString(SI_VOTANS_FILET_FISH_ALL, "Чистить всё")
+SafeAddString(SI_KEYBINDINGS_CATEGORY_FILET_FISH, "Почистить рыбу")
+SafeAddString(SI_BINDING_NAME_VOTANS_FISH_FILLET_ALL_STACKS, "Почистить всю рыбу")
+SafeAddString(SI_VOTANS_FILET_OPT_ALLSTACKS, "Показывать \"Почистить всю рыбу\"")
+SafeAddString(SI_VOTANS_FILET_OPT_ALLSTACKS_TOOLTIP, "Если включено, Votan's Fish Fillet почистить все стаки рыбы, которые есть у вас в инвентаре.")
+SafeAddString(SI_VOTANS_FILET_OPT_ALLSTACKS_ALWAYS, "Всегда чистить все стаки")
+SafeAddString(SI_VOTANS_FILET_OPT_ALLSTACKS_ALWAYS_TOOLTIP, "Если включено, Votan's Fish Fillet всегда будет чистить все стаки рыбы в вашем инвентаре.")
